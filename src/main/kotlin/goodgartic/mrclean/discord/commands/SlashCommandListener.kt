2e5f9e7c2d67a8313ad28aef8a68cedd532c0e32
Jiří Vrba <jiri@vrba.dev>
2021-12-30 10:44:37 +0100

    Implement onSlashCommand event handler

--- src/main/kotlin/goodgartic/mrclean/discord/commands/SlashCommandListener.kt
+++ src/main/kotlin/goodgartic/mrclean/discord/commands/SlashCommandListener.kt
@@ -1,8 +1,11 @@
 package goodgartic.mrclean.discord.commands
 
 import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.events.application.ApplicationCommandCreateEvent
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import org.springframework.stereotype.Component
+import java.lang.IllegalStateException
 
 @Component
 class SlashCommandListener(private val guild: Guild, private val commands: List<SlashCommand>) : ListenerAdapter() {
@@ -15,5 +18,13 @@ class SlashCommandListener(private val guild: Guild, private val commands: List<
         guild.updateCommands()
             .addCommands(commands.map { it.definition })
             .queue()
+    }
+
+    override fun onSlashCommand(event: SlashCommandEvent) {
+        val handler = commands.firstOrNull { command -> command.definition.name == event.name }
+            ?: throw IllegalStateException("Cannot find handler for slash command /${event.name}")
+
+            // TODO: Add exception handling, permissions stuff etc...
+            handler.execute(event)
     }
 }

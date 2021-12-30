a3e7274170160fbe1586a65c53f4eb1be83697e1
Jiří Vrba <jiri@vrba.dev>
2021-12-30 12:57:00 +0100

    Formatting

--- src/main/kotlin/goodgartic/mrclean/commands/SlashCommandListener.kt
+++ src/main/kotlin/goodgartic/mrclean/commands/SlashCommandListener.kt
@@ -4,7 +4,6 @@ import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import org.springframework.stereotype.Component
-import java.lang.IllegalStateException
 
 @Component
 class SlashCommandListener(private val guild: Guild, private val commands: List<SlashCommand>) : ListenerAdapter() {
@@ -23,7 +22,7 @@ class SlashCommandListener(private val guild: Guild, private val commands: List<
         val handler = commands.firstOrNull { command -> command.definition.name == event.name }
             ?: throw IllegalStateException("Cannot find handler for slash command /${event.name}")
 
-            // TODO: Add exception handling, permissions stuff etc...
-            handler.execute(event)
+        // TODO: Add exception handling, permissions stuff etc...
+        handler.execute(event)
     }
 }

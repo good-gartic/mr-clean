f541decf0ba319f38fea2046d034859f5505c13f
Jiří Vrba <jiri@vrba.dev>
2021-12-30 17:04:18 +0100

    Implement filter specification for filtering channels / users / roles

--- src/main/kotlin/goodgartic/mrclean/entities/Filter.kt
+++ src/main/kotlin/goodgartic/mrclean/entities/Filter.kt
@@ -18,30 +18,35 @@ data class Filter(
     val pattern: String,
 
     /**
-     * Limit this filter to a specific channel(s), if empty, message in all channels are filtered
-     */
-    val channels: String = "",
-
-    /**
-     * Limit this filter to a specific user(s), if empty, messages from all users are filtered
-     */
-    val users: String = "",
-
-    /**
-     * Limit this filter to a specific role(s), if empty, messages from all roles are filtered
-     */
-    val roles: String = "",
-
-    /**
      * Delay in seconds, before the filtered message is deleted
      */
     val delay: Int = 0,
 
     /**
+     * Limit this filter to a specific channel(s), if empty, message in all channels are filtered
+     */
+    private val channels: String = "",
+
+    /**
+     * Limit this filter to a specific user(s), if empty, messages from all users are filtered
+     */
+    private val users: String = "",
+
+    /**
+     * Limit this filter to a specific role(s), if empty, messages from all roles are filtered
+     */
+    private val roles: String = "",
+
+    /**
      * IDs of the channel, that the message should be reposted to, if empty, the message is just deleted
      */
-    val repostChannels: String = "",
+    private val repostChannels: String = "",
 ) {
+    fun channels(): FilterSpecification = channels.toFilterSpecification()
+    fun users(): FilterSpecification = users.toFilterSpecification()
+    fun roles(): FilterSpecification = roles.toFilterSpecification()
+    fun repostChannels(): List<String> = repostChannels.split(",")
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
---
+++ src/main/kotlin/goodgartic/mrclean/entities/FilterSpecification.kt
@@ -0,0 +1,26 @@
+package goodgartic.mrclean.entities
+
+/**
+ * Convert a comma-separated list of entities, that are either allowed or denied (when prefixed with an !),
+ * to a filter specification instance allowing matching entries
+ */
+fun String.toFilterSpecification(): FilterSpecification =
+    if (this.isBlank()) FilterSpecification()
+    else this.split(",").partition { it.startsWith("!") }
+        .let { (denied, allowed) ->
+            FilterSpecification(allowed, denied.map { it.removePrefix("!") })
+        }
+
+data class FilterSpecification(
+    val allowedEntities: List<String> = emptyList(),
+    val deniedEntities: List<String> = emptyList(),
+) {
+    fun allows(entry: String): Boolean = when {
+        // If the entry was explicitly denied
+        entry in deniedEntities -> false
+        // If the entry was explicitly allowed
+        entry in allowedEntities -> true
+        // If allowed entities are empty, allow all entities, otherwise limit the specification to whitelist-only
+        else -> allowedEntities.isEmpty()
+    }
+}

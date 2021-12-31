b6aed6cc0a65573a2ff5d2697866ffcb29c5ce1c
Jiří Vrba <jiri@vrba.dev>
2021-12-31 10:45:17 +0100

    Improve filter rendering format

--- src/main/kotlin/goodgartic/mrclean/extensions/FilterExtensions.kt
+++ src/main/kotlin/goodgartic/mrclean/extensions/FilterExtensions.kt
@@ -4,22 +4,23 @@ import goodgartic.mrclean.entities.Filter
 import goodgartic.mrclean.entities.FilterSpecification
 
 fun FilterSpecification.description(name: String, transform: (String) -> String): String =
-    if (this.isEmpty()) "Applies for all $name"
+    if (this.isEmpty()) "_Applies for all ${name}_"
     else {
         val allowed = this.allowedEntities.joinToString(", ") { transform(it) }
         val denied = this.deniedEntities.joinToString(", ") { transform(it) }
 
         """
-                Applies for those $name: $allowed,
-                Does not applies for those $name: $denied
-                """.trimIndent()
+        Applies for those $name: $allowed,
+        Does not applies for those $name: $denied
+        """.trimIndent()
     }
 
 fun Filter.description(): String =
     """
-    Pattern: `${this.pattern.replace("`", "\\`")}`
-    Delay:    ${if (this.delay <= 0) "Applied immediately" else "${this.delay} seconds"}
-    Channels: ${this.channels().description("channels") { channel -> "<#$channel>" }}
-    Users:    ${this.users().description("users") { user -> "<@$user>" }}
-    Roles:    ${this.roles().description("roles") { role -> "<@&$role>" }}
+    **ID**: `${this.id}`
+    **Pattern**: `${this.pattern.replace("`", "\\`")}`
+    **Delay**:    ${if (this.delay <= 0) "_Applied immediately_" else "Applied after **${this.delay} seconds**"}
+    **Channels**: ${this.channels().description("channels") { channel -> "<#$channel>" }}
+    **Users**:    ${this.users().description("users") { user -> "<@$user>" }}
+    **Roles**:    ${this.roles().description("roles") { role -> "<@&$role>" }}
     """.trimIndent()

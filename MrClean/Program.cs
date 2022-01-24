3a3b9ad0a649c56e164c57d2033506737cff9809
Jiří Vrba <jiri@vrba.dev>
2022-01-24 17:17:47 +0100

    Add user secrets configuration support

--- MrClean/Program.cs
+++ MrClean/Program.cs
@@ -2,6 +2,15 @@ using MrClean;
 using MrClean.Services;
 
 var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder.AddEnvironmentVariables();
+
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            builder.AddUserSecrets<Program>();
+        }
+    })
     .ConfigureServices(services =>
     {
         services.AddHostedService<DiscordBotService>();

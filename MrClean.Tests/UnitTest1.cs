892dc28752a0c2a6754e8559611b00d24006c623
Jiří Vrba <jiri@vrba.dev>
2022-01-25 17:51:02 +0100

    Add new unit testing project to the solution

---
+++ MrClean.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace MrClean.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

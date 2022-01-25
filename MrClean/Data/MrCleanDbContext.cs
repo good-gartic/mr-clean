f1779bea731adba0316fd8dde9fe5fc04e067966
Jiří Vrba <jiri@vrba.dev>
2022-01-25 17:50:45 +0100

    Create initial migration for message filters

--- MrClean/Data/MrCleanDbContext.cs
+++ MrClean/Data/MrCleanDbContext.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using MrClean.Models;
 
 namespace MrClean.Data;
 
+#nullable enable
 public class MrCleanDbContext : DbContext
 {
     public MrCleanDbContext(DbContextOptions<MrCleanDbContext> options) : base(options)
     {
     }
+
+    public virtual DbSet<MessageFilter> MessageFilters { get; set; }
 }
---
+++ MrClean/Migrations/20220125164931_MessageFilters.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MrClean.Migrations
+{
+    public partial class MessageFilters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessageFilters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Delay = table.Column<long>(type: "bigint", nullable: false),
+                    Pattern = table.Column<string>(type: "text", nullable: true),
+                    RepostChannelId = table.Column<decimal>(type: "numeric(20,0)", nullable: true),
+                    ChannelsSpecification = table.Column<string>(type: "text", nullable: true),
+                    UsersSpecification = table.Column<string>(type: "text", nullable: true),
+                    RolesSpecification = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageFilters", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageFilters");
+        }
+    }
+}

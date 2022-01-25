6c2b58defd33c659c142ac62479b3f17389fe268
Jiří Vrba <jiri@vrba.dev>
2022-01-25 12:35:31 +0100

    Migrate pending database migrations on bot startup

--- MrClean/Services/DiscordBotService.cs
+++ MrClean/Services/DiscordBotService.cs
@@ -1,7 +1,9 @@
 using Discord;
 using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using MrClean.Configuration;
+using MrClean.Data;
 
 namespace MrClean.Services;
 
@@ -13,15 +15,22 @@ public class DiscordBotService : BackgroundService
 
     private readonly DiscordSocketClient _client;
 
-    public DiscordBotService(ILogger<DiscordBotService> logger, IOptions<DiscordOptions> options)
+    private readonly IDbContextFactory<MrCleanDbContext> _contextFactory;
+
+    public DiscordBotService(ILogger<DiscordBotService> logger, IOptions<DiscordOptions> options, IDbContextFactory<MrCleanDbContext> contextFactory)
     {
         _logger = logger;
+        _contextFactory = contextFactory;
         _options = options.Value; 
         _client = new DiscordSocketClient();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Preparing the database");
+
+        await UpdateDatabase();
+        
         _logger.LogInformation("Starting the Mr.Clean Discord bot at {time}", DateTimeOffset.Now);
 
         _client.Ready += HandleReadyEvent;
@@ -29,6 +38,12 @@ public class DiscordBotService : BackgroundService
         await _client.LoginAsync(TokenType.Bot, _options.Token);
         await _client.StartAsync();
         await Task.Delay(-1, stoppingToken);
+    }
+
+    private async Task UpdateDatabase()
+    {
+        await using var context = await _contextFactory.CreateDbContextAsync();
+        await context.Database.MigrateAsync();
     }
 
     private async Task HandleReadyEvent()
